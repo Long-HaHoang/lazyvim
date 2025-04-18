5d531183f4aa96bd8098ac95a0e98cedced2f223
Long Ha Hoang <long.ha@privadsy.com>
2025-04-18 21:31:23 +0200

    refactor: opt to grouping dictionary

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,16 +2,13 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
---- Disable hiding of markup
-vim.opt.conceallevel = 0
+-- Set multiple options at once
+vim.opt = {
+  conceallevel = 0, -- Disable hiding of markup
+  wrap = true, -- Break lines at word boundaries
+  linebreak = true, -- Break lines at word boundaries (better wrapping)
+  autoindent = true, -- Copy indent of current line when inserting new line
+}
 
---- Break lines at word boundaries
-vim.opt.wrap = true
-vim.opt.linebreak = true
-
---- Copy the indent of the current line when inserting a new line
-vim.opt.autoindent = true
-
---- Disable the inlay hints (additional information about types and parameters names) because it clutters my view
-
-vim.g.lazyvim_no_inlay_hints = true
+-- Disable the inlay hints (additional information about types and parameter names) because it clutters my view
+vim.g.lazyvim_no_inlay_hints = false
